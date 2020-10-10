fa97fdb85973394ad99fbe953b96aa29db92bf06
alex <gzane24@hotmail.com>
2020-10-10 22:24:08 +0800

    feat: add cluster feature (#9)
    Co-authored-by: alex <alex@alex.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,25 @@
+## 1.0.1-rc.5 (2020-10-10)
+
+
+### Bug Fixes
+
+* add useExisting for custom provider ([ea84c42](https://github.com/augejs/module-core/commit/ea84c4274d2a9de53724024d72e1de556d9d0488))
+* refactor from hook util ([#1](https://github.com/augejs/module-core/issues/1)) ([f2b8046](https://github.com/augejs/module-core/commit/f2b804637cf82589079d5a8aef2dbe3108368b69))
+* refactor life cycle hook ([#4](https://github.com/augejs/module-core/issues/4)) ([1f078ab](https://github.com/augejs/module-core/commit/1f078ab9b7a95a9cff79f6d2bea87e53c55219cb))
+* refactor life cycle hook ([#4](https://github.com/augejs/module-core/issues/4)) ([#5](https://github.com/augejs/module-core/issues/5)) ([b8fd4a2](https://github.com/augejs/module-core/commit/b8fd4a2385497e8a258f7b880ff976e5f8228e32))
+* refactor life cycle hook ([#4](https://github.com/augejs/module-core/issues/4)) ([#5](https://github.com/augejs/module-core/issues/5)) ([#6](https://github.com/augejs/module-core/issues/6)) ([557c6b1](https://github.com/augejs/module-core/commit/557c6b1a0e046c8b50ec5550c211d1c7293aec0b))
+* update @augejs/provider-scanner ver ([92a0dcc](https://github.com/augejs/module-core/commit/92a0dcce98de97ba010607419f567e3fda0fa87e))
+* update ver to 0.0.33 ([d1f1f34](https://github.com/augejs/module-core/commit/d1f1f34dad9b981ff2f900ddc88dc8d6a41657ea))
+
+
+### Features
+
+* add cluster feature ([ef011c6](https://github.com/augejs/module-core/commit/ef011c6ba086faaadcbcc76907368d0c4bbfd4e5))
+* add cluster feature ([#8](https://github.com/augejs/module-core/issues/8)) ([260da2f](https://github.com/augejs/module-core/commit/260da2f482571f7629dc7c4319f7308665128ded))
+* release for 1.0.0-rc.3 ([#7](https://github.com/augejs/module-core/issues/7)) ([72c6e87](https://github.com/augejs/module-core/commit/72c6e87fe05675e1ffec1fd0189cc2519620496e))
+
+
+
 ## 1.0.1-rc.4 (2020-10-07)
 
 ### Bug Fixes
--- src/decorators/Cluster.decorator.ts
+++ src/decorators/Cluster.decorator.ts
@@ -1,67 +1,115 @@
 import os from 'os';
 import cluster, { Worker } from 'cluster';
-
 import { IScanNode, Metadata } from '@augejs/provider-scanner';
-import { ScanHook } from './ScanHook.decorator';
+import { LifecycleOnAppWillCloseHook, Lifecycle__onAppReady__Hook } from './LifecycleHook.decorator';
+import { Logger } from '../logger';
+import { Module } from './Module.decorator';
+
+const logger = Logger.getLogger('cluster');
 
 type ClusterOptions = {
   workers: number
-  disabled?: boolean
+  enable?: boolean
+  clusterModule?: Function
 };
 
-export function Cluster (opts?:ClusterOptions):ClassDecorator {
+@Module()
+export class DefaultClusterModule {}
+
+export function Cluster(opts?:ClusterOptions): ClassDecorator {
   opts = {
     workers: 0,
-    disabled: false,
+    enable: true,
     ...opts,
   };
 
   return function(target: Function) {
-    Metadata.decorate([
-      ScanHook(async (scanNode: IScanNode, next: Function) => {
-        if (opts!.disabled) return;
-
-        if (cluster.isMaster) {
-          const cpuCount: number = os.cpus().length;
-          let workers: number = opts?.workers || 0;
-          if (workers < 0) {
-            workers = Math.abs(Math.trunc(workers));
-            workers = Math.min(workers, cpuCount);
-          } else if (workers > 0) {
-            workers = workers;
-          } else {
-            workers = cpuCount;
-          }
-
-          for(let i = 0; i < workers; i++) {
-            cluster.fork();
-          }
-
-          cluster.on('exit', (worker: Worker, code: number, signal: string) => {
-            if (code !== 0 && !worker.exitedAfterDisconnect) {
-              cluster.fork();
-            }
-          })
-          // end of master
-        }
-
-        if (cluster.worker) {
-
-        }
-      })
-    ], target);
     Cluster.defineMetadata(target, opts!);
   }
-}
-
-Cluster.defineMetadata = (target: Object, opts: ClusterOptions)=> {
-  Metadata.defineMergeObjectMetadata(Cluster, opts, target);
 }
 
 Cluster.hasMetadata = (target: Object): boolean => {
   return Metadata.hasMetadata(Cluster, target)
 }
 
+Cluster.defineMetadata = (target: Object, opts: ClusterOptions)=> {
+  Metadata.defineMetadata(Cluster, opts, target);
+}
+
 Cluster.getMetadata = (target: object):ClusterOptions => {
-  return Metadata.getMetadata(Cluster, target) || {};
+  return Metadata.getMetadata(Cluster, target) || {
+    workers: 0
+  };
+}
+
+Cluster.DefaultClusterModule = DefaultClusterModule;
+
+Cluster.ClusterMasterClassDecorator = function (opts?:ClusterOptions):ClassDecorator {
+  opts = {
+    workers: 0,
+    ...opts,
+  };
+
+  return function(target: Function) {
+    Metadata.decorate([
+      Lifecycle__onAppReady__Hook(async (scanNode: IScanNode, next: Function) => {
+        const cpuCount: number = os.cpus().length;
+        let workers: number = opts?.workers || parseInt(process.env.WORKERS || '0') || 0;
+        if (workers < 0) {
+          workers = Math.abs(Math.trunc(workers));
+          workers = Math.min(workers, cpuCount);
+        } else if (workers > 0) {
+          workers = workers;
+        } else {
+          workers = cpuCount;
+        }
+
+        await Promise.all(
+          Array.from(new Array(workers)).map(()=>{
+            return new Promise((resolve: Function, reject: Function) => {
+              const worker = cluster.fork()
+              .once('exit', (code: number, signal: string) => {
+                reject(new Error(`worker(pid: ${worker.process.pid}) Boot Error`));
+              })
+              .once('message', (message: any)=> {
+                if (message?.cmd === '__onAppReady__') {
+                  worker.removeAllListeners('exit');
+                  resolve();
+                }
+              })
+            })
+          })
+        )
+
+        cluster.on('exit', (worker: Worker, code: number, signal: string) => {
+          if (!worker.exitedAfterDisconnect) {
+            logger.info(`worker(pid: [${worker.process.pid}]) is exit. code: ${code} signal: ${signal} and will be forked new process`);
+            cluster.fork();
+          }
+        });
+        await next();
+      }),
+
+      LifecycleOnAppWillCloseHook(async (scanNode: IScanNode, next: Function) => {
+        cluster.removeAllListeners('exit');
+        await Promise.all(Object.values(cluster.workers).map((worker?: Worker) => {
+          return new Promise((resolve: Function) => {
+            if (!worker) {
+              resolve();
+              return;
+            }
+            worker.once('exit', ()=>{
+              logger.warn(`worker(pid: [${worker.process.pid}]) is exited with signal: SIGTERM`);
+              resolve();
+            });
+
+            worker.process.kill('SIGTERM');
+            logger.info(`worker(pid: [${worker.process.pid}]) is killed with signal: SIGTERM`);
+          })
+        }))
+
+        await next();
+      })
+    ], target);
+  }
 }
